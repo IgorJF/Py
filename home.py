5da362332b6ecbf30ddcedc5db3043093cbac12f
IgorJF <111748228+IgorJF@users.noreply.github.com>
2023-11-01 18:37:01 -0300

    Cadastrar e Logar na Plataforma

--- home.py
+++ home.py
@@ -1,35 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
-driver = webdriver.Chrome()
+driver = webdriver.ChromiumEdge()
 driver.get("http://weka.inf.ufes.br/IFESTP/index.php")
 
-login = driver.find_element(By.ID, "username")
-login.clear()
-login.send_keys("Botafogo")
-login.send_keys(Keys.RETURN) #se eu tirar sozinho não muda nada, mas se eu tirar os dois não loga
+#Cadastro e Login na plataforma, caso seja necessário.
+'''
+username = driver.find_element(By.ID, "username")
+username.clear()
+username.send_keys("EduardoOPato")
+username.send_keys(Keys.RETURN) #se eu tirar sozinho não muda nada, mas se eu todos não loga
 
 email = driver.find_element(By.ID, "email")
 email.clear()
-email.send_keys("botafogo1904@campeao.com")
-email.send_keys(Keys.RETURN) #se eu tirar sozinho não muda nada, mas se eu tirar os dois não loga
+email.send_keys("eduardoopato@campeao.com")
+email.send_keys(Keys.RETURN) 
 
-password = driver.find_element(By.ID, "password")
+password = driver.find_element(By.NAME, "password")
 password.clear()
-password.send_keys("botafogo1904")
-password.send_keys(Keys.RETURN) 
+password.send_keys("eduopato0123")
 
-password_2 = driver.find_element(By.ID, "passwordConfirm")
+password_2 = driver.find_element(By.NAME, "passwordConfirm")
 password_2.clear()
-password_2.send_keys("botafogo1904")
-password_2.send_keys(Keys.RETURN) 
+password_2.send_keys("eduopato0123")
 
 button = driver.find_element(By.NAME, "submit")
 button.send_keys(Keys.RETURN) #se eu tirar não loga 
 
-login = driver.find_element_by_xpath('//a[text()="Login"]')
+time.sleep(5)
+
+login = driver.find_element(By.TAG_NAME, "a")
 login.click()
+
+username_login = driver.find_element(By.ID, "username")
+username_login.clear()
+username_login.send_keys("EduardoOPato")
+username_login.send_keys(Keys.RETURN) 
+
+password_login = driver.find_element(By.ID, "password")
+password_login.clear()
+password_login.send_keys("eduopato0123")
+password_login.send_keys(Keys.RETURN) 
+
+time.sleep(3)
+'''
 
 assert "No results found." not in driver.page_source
 driver.close()
